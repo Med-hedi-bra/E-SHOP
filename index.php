6a65faa31640585517685ce2560690b5c4848054
MEd Hedi Bra <mohame>
2023-02-01 01:33:40 +0100

    lll

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (!empty($_POST)) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>E-SHOP-TEST-REMOTE</title>
+  <title>E-SHOP-TEST-REMOTE-der</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <!--integration de bootstrap styles-->
   <link rel="stylesheet" href="./styles/style_index.css">
